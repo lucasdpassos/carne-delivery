fcc7c1f49cbd970e163e88bdf627fe8af340cd70
Lucas Passos <lucas.depassos@yahoo.com.br>
2021-09-23 11:49:28 -0300

    slug on app config changes

---
+++ src/Screens/Franchisee/index.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ImageBackground, Image, Pressable, TouchableOpacity, TextInput, CheckBox } from 'react-native';
+import 'react-native-gesture-handler';
+
+
+
+
+
+export default function App() {
+
+  
+
+  return (
+    <View style={styles.container}>
+       
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    
+    backgroundColor: '#f2f2f2',
+    alignItems: 'center',    
+    zIndex:-1
+  }
+ 
+ 
+});
--- src/Stacks/MainStack.js
+++ src/Stacks/MainStack.js
@@ -4,13 +4,14 @@ import { createStackNavigator } from '@react-navigation/stack'
 import Welcome from '../Screens/Welcome/index'
 import Store from '../Screens/Store/index'
 import Login from '../Screens/Login/index'
+import Franchisee from '../Screens/Franchisee/index'
 
 
 const Stack = createStackNavigator()
 
 export default () => (
     <Stack.Navigator 
-    initialRouteName="Login"
+    initialRouteName="Franchisee"
     screenOptions ={{
         headerShown: false
     }}
@@ -18,6 +19,7 @@ export default () => (
         <Stack.Screen name="Welcome" component={Welcome} / >
         <Stack.Screen name="Store" component={Store} / >
         <Stack.Screen name="Login" component={Login} / >
+        <Stack.Screen name="Franchisee" component={Franchisee} / >
        
     </Stack.Navigator>
 )
